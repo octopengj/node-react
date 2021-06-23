d7fdc762927ddc4e82365f6355f9b98b9cb29bbf
octopengj <octopengj@gmail.com>
2021-06-24 05:04:22 +0900

    node&react

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { User} = require("./models/User");
-
+const {User} = require("./models/User");
+const {auth} = require('./middleware/auth');
 const config = require('./config/key');
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 })
 
 
-app.post('/register', (req, res) =>{
+app.post('/api/users/register', (req, res) =>{
   const user = new User(req.body)
 
   user.save((err, userInfo) => {
@@ -62,6 +62,33 @@ app.post('/login', (req, res) => {
             })
         })
     })
+})
+
+app.get('api/users/auth', auth, (req, res)=> {
+
+    // 여기까지 미들웨어를 통과해 왔다는 예기는 Authentication이 true라는 말
+    res.status(200).json({
+        // 유저 정보를 제공
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id}, 
+        {token:""}
+        , (err, user) => {
+            if(err) return res.json({success: false, err});
+            return res.status(200).send({
+                success: true
+            })       
+        })
 })
 
 app.listen(port, () => {
