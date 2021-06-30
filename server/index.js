d88c040f78ed420fb6bb1359972de639b4b7e493
octopengj <octopengj@gmail.com>
2021-07-01 01:06:11 +0900

    node&react

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.post('/api/users/register', (req, res) =>{
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('api/users/login', (req, res) => {
 
     //요청된 이메일을 데이터베이스에서 있는지 찾는다.
     user.findOne({email: req.body.email}, (err, user) => {
